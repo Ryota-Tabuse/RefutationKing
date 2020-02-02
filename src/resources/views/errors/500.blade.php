2f3988889c0e58f46f8ee4d5837a215c4c8998bf
Ryota-Tabuse <bububutase.0107@gmail.com>
2020-02-02 23:50:38 +0900

    エラー時遷移画面作成

---
+++ src/resources/views/errors/500.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Scripts -->
+	@if(app('env') == 'production')
+	<link href="{{ secure_asset('css/styles.css') }}" rel="stylesheet">
+	<link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
+	@else
+	<link href="{{ asset('css/styles.css') }}" rel="stylesheet">
+	<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+	@endif
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div style="margin: 100px auto;">
+                <div class="text-center">
+                    <p>予期せぬエラーが発生しました。</p>
+                    <a href="{{ route('home') }}" class="btn">ホームへ戻る</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
