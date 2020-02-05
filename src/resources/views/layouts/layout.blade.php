92347e7db32f3267debe73c39000ab9d27585280
Ryota-Tabuse <bububutase.0107@gmail.com>
2020-02-06 01:08:44 +0900

    ヘッダーレイアウト修正

--- src/resources/views/layouts/layout.blade.php
+++ src/resources/views/layouts/layout.blade.php
@@ -38,13 +38,11 @@
 					</button>
 
 					<div class="collapse navbar-collapse" id="navbarSupportedContent">
-						<!-- Left Side Of Navbar -->
+
 						<ul class="navbar-nav mr-auto">
 						</ul>
 
-						<!-- Right Side Of Navbar -->
 						<ul class="navbar-nav ml-auto">
-							<!-- Authentication Links -->
 							@guest
 							<li class="nav-item">
 								<a class="nav-link" href="{{ route('login') }}">{{ __('ログイン') }}</a>
@@ -87,6 +85,8 @@
 	@section('footer')
 	<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 	<script src="https://js.pusher.com/5.0/pusher.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
 
 	@show
 </body>
